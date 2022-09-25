db7960e6aef3dcffaba796e25db628b52d5e89fd
HarDev <53939102+KortezJatkov@users.noreply.github.com>
2022-09-25 19:45:32 +0300

    Update Component.cs

--- PacMan/Component.cs
+++ PacMan/Component.cs
@@ -9,7 +9,6 @@ using System.Windows.Forms;
 using System.IO;
 using System.Numerics;
 using System.Collections.ObjectModel;
-//using static PacMan.PacManComponent;
 
 namespace PacMan
 {
@@ -38,40 +37,7 @@ namespace PacMan
 
         private string _MapPath = "";
 
-        private int[,] _NavMap =
-            {
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-            };
+        private int[,] _NavMap = new int[31, 28];
 
         private Player _Player = null;
 
@@ -871,13 +837,12 @@ namespace PacMan
             var _CurrentRayPos = new Transform(in_Start.X, in_Start.Y, 14, 14);
             do
             {
-                _CurrentRayPos = new Transform((int)(in_Start.X + (in_Destination.X - in_Start.X) * _Times), (int)(in_Start.Y + (in_Destination.Y - in_Start.Y) * _Times), 14, 14);
+                _CurrentRayPos = new Transform((int)(in_Start.X + (in_Destination.X - in_Start.X) * _Times), (int)(in_Start.Y + (in_Destination.Y - in_Start.Y) * _Times), 1, 1);
                 foreach(Wall _CurrentWall in IncomeWallList)
                 {
                     if(isOverlaping(_CurrentRayPos, _CurrentWall.Transform) == true)
                     {
                         _Result = false;
-                        //Console.WriteLine(_CurrentRayPos.X + "x" + _CurrentRayPos.Y +" vs "+_CurrentWall.Transform.X+"x"+_CurrentWall.Transform.Y);
                     }
                     
                 }
@@ -895,7 +860,6 @@ namespace PacMan
             do { _RandomPatrolPoint = new Point(_Random.Next(1, 28), _Random.Next(1, 31)); }
             while (this._NavMap[_RandomPatrolPoint.Y, _RandomPatrolPoint.X] == 2);
 
-            Console.WriteLine(_RandomPatrolPoint);
             return _RandomPatrolPoint;
         }
 
@@ -1201,10 +1165,8 @@ namespace PacMan
         {
             public static List<Point> FindPath(int[,] field, Point start, Point goal)
             {
-                // Шаг 1. Создается 2 списка вершин — ожидающие рассмотрения и уже рассмотренные. В ожидающие добавляется точка старта, список рассмотренных пока пуст.
                 var closedSet = new Collection<PathNode>();
                 var openSet = new Collection<PathNode>();
-                // Шаг 2. Для каждой точки рассчитывается F = G + H. G — расстояние от старта до точки, H — примерное расстояние от точки до цели. О расчете этой величины я расскажу позднее. Так же каждая точка хранит ссылку на точку, из которой в нее пришли.
                 PathNode startNode = new PathNode()
                 {
                     Position = start,
@@ -1215,35 +1177,27 @@ namespace PacMan
                 openSet.Add(startNode);
                 while (openSet.Count > 0)
                 {
-                    // Шаг 3. Из списка точек на рассмотрение выбирается точка с наименьшим F. Обозначим ее X.
                     var currentNode = openSet.OrderBy(node => node.EstimateFullPathLength).First();
-                    // Шаг 4. Если X — цель, то мы нашли маршрут.
                     if (currentNode.Position == goal)
                         return GetPathForNode(currentNode);
-                    // Шаг 5. Переносим X из списка ожидающих рассмотрения в список уже рассмотренных.
                     openSet.Remove(currentNode);
                     closedSet.Add(currentNode);
-                    // Шаг 6. Для каждой из точек, соседних для X (обозначим эту соседнюю точку Y), делаем следующее:
                     foreach (var neighbourNode in GetNeighbours(currentNode, goal, field))
                     {
-                        // Шаг 7. Если Y уже находится в рассмотренных — пропускаем ее.
                         if (closedSet.Count(node => node.Position == neighbourNode.Position) > 0)
                             continue;
                         var openNode = openSet.FirstOrDefault(node =>
                           node.Position == neighbourNode.Position);
-                        // Шаг 8. Если Y еще нет в списке на ожидание — добавляем ее туда, запомнив ссылку на X и рассчитав Y.G (это X.G + расстояние от X до Y) и Y.H.
                         if (openNode == null)
                             openSet.Add(neighbourNode);
                         else
                           if (openNode.PathLengthFromStart > neighbourNode.PathLengthFromStart)
                         {
-                            // Шаг 9. Если же Y в списке на рассмотрение — проверяем, если X.G + расстояние от X до Y < Y.G, значит мы пришли в точку Y более коротким путем, заменяем Y.G на X.G + расстояние от X до Y, а точку, из которой пришли в Y на X.
                             openNode.CameFrom = currentNode;
                             openNode.PathLengthFromStart = neighbourNode.PathLengthFromStart;
                         }
                     }
                 }
-                // Шаг 10. Если список точек на рассмотрение пуст, а до цели мы так и не дошли — значит маршрут не существует.
                 return null;
             }
 
@@ -1261,7 +1215,6 @@ namespace PacMan
             {
                 var result = new Collection<PathNode>();
 
-                // Соседними точками являются соседние по стороне клетки.
                 Point[] neighbourPoints = new Point[4];
                 neighbourPoints[0] = new Point(pathNode.Position.X + 1, pathNode.Position.Y);
                 neighbourPoints[1] = new Point(pathNode.Position.X - 1, pathNode.Position.Y);
@@ -1270,15 +1223,12 @@ namespace PacMan
 
                 foreach (var point in neighbourPoints)
                 {
-                    // Проверяем, что не вышли за границы карты.
                     if (point.X < 0 || point.X >= field.GetLength(0))
                         continue;
                     if (point.Y < 0 || point.Y >= field.GetLength(1))
                         continue;
-                    // Проверяем, что по клетке можно ходить.
                     if ((field[point.X, point.Y] != 0) && (field[point.X, point.Y] != 1))
                         continue;
-                    // Заполняем данные для точки маршрута.
                     var neighbourNode = new PathNode()
                     {
                         Position = point,
